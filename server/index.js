2af0146590dd6bf703cee30f75a7331d8b94b07b
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-10-13 10:57:48 -0400

    Removed express, changed server to standard node format

--- server/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import test from "./routes/app";
-const app = express();
-const PORT = 3000;
-
-app.use('/', test);
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- server/routes/app.js
+++
@@ -1,7 +0,0 @@
-import { defaultCipherList } from "constants";
-import { Router } from "express";
-const router = Router();
- 
-router.get('/test', (req, res) => res.send('Hello World!'));
-
-export default router;
