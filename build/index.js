3a025206b818bb233c7bd19d1d5eaf58ed5f3629
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-10-14 10:04:18 -0400

    Small Change

--- build/index.js
+++ build/index.js
@@ -6,12 +6,19 @@ var _fs = require("fs");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-(0, _nodeFetch["default"])('https://www.reddit.com/r/pics/search.json?q=programming&sort=new').then(function (res) {
-  return res.json();
-}).then(function (res) {
-  return _fs.promises.writeFile('data.json', JSON.stringify(res));
-}).then(function () {
-  return console.log('Saved Successfully');
-})["catch"](function (err) {
-  return console.error(err);
-});
+var searchData = function searchData(topic) {
+  var sort = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "new";
+  (0, _nodeFetch["default"])("https://www.reddit.com/r/pics/search.json?q=".concat(topic, "&sort=").concat(sort)).then(function (res) {
+    return res.json();
+  }).then(function (res) {
+    console.log(res);
+
+    _fs.promises.writeFile('data.json', JSON.stringify(res));
+  }).then(function () {
+    return console.log('Saved Successfully');
+  })["catch"](function (err) {
+    return console.error(err);
+  });
+};
+
+searchData('Gaming');
