3e6a78907192c426fe06bb6d580a68f33765d9e3
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-10-14 09:38:05 -0400

    Refactored then chaining to one level

--- build/index.js
+++ build/index.js
@@ -2,20 +2,16 @@
 
 var _nodeFetch = _interopRequireDefault(require("node-fetch"));
 
-var _fs = _interopRequireDefault(require("fs"));
+var _fs = require("fs");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 (0, _nodeFetch["default"])('https://www.reddit.com/r/pics/search.json?q=programming&sort=new').then(function (res) {
   return res.json();
 }).then(function (res) {
-  var data = JSON.stringify(res);
-
-  _fs["default"].promises.writeFile('data.json', data).then(function () {
-    return console.log('Saved Successfully');
-  })["catch"](function (err) {
-    throw err;
-  });
+  return _fs.promises.writeFile('data.json', JSON.stringify(res));
+}).then(function () {
+  return console.log('Saved Successfully');
 })["catch"](function (err) {
   return console.error(err);
 });
